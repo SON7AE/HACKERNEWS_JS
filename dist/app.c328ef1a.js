57c365280097ca90f99cc5c4b01031fc1ec3d20d
SON7AE <bobsot0225@gmail.com>
2023-02-11 15:51:32 +0900

    08. 댓글 목록이 표시되는 아름다운 UI 만들기

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -137,9 +137,9 @@ function newsFeed() {
   // 메인 페이지
   var NEWSFEED = getData(NEWS_URL);
   var NEWSLIST = [];
-  var template = "\n        <div class=\"container mx-auto p-4\">\n            <h1>HACKER NEWS</h1>\n            <ul>\n                {{__news_feed__}}\n            </ul>\n            <div>\n                <a href=\"#\">{{__prev_page__}}</a>\n                <a href=\"#\">{{__next_page__}}</a>\n            </div>\n        </div>\n    ";
+  var template = "\n        <div class=\"bg-gray-600 min-h-screen\">\n            <div class=\"bg-white text-xl\">\n                <div class=\"mx-auto px-4\">\n                    <div class=\"flex justify-between items-center py-6\">\n                        <div class=\"flex justify-start\"> \n                            <h1 class=\"font-extrabold\">HACKER NEWS</h1>\n                        </div>\n                        <div class=\"items-center justify-end\">\n                            <a href=\"#/page/{{__prev_page__}}\" class=\"text-gray-500\">\n                                Previous\n                            </a>\n                            <a href=\"#/page/{{__next_page__}}\" class=\"text-gray-500 ml-4\">\n                                Next\n                            </a>\n                        </div>    \n                    </div>\n                </div>\n            </div>            \n            <div class=\"p-4 text-2xl text-gray-700\">\n                {{__news_feed__}}\n            </div>\n        </div>\n    ";
   for (var i = (STORE.currentPage - 1) * 10; i < STORE.currentPage * 10; i++) {
-    NEWSLIST.push("\n        <li>\n            <a href=\"#/show/".concat(NEWSFEED[i].id, "\">\n                ").concat(NEWSFEED[i].title, " (").concat(NEWSFEED[i].comments_count, ") \n            </a> \n        </li>\n    "));
+    NEWSLIST.push("\n            <div class=\"p-6 bg-white mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100\">\n                <div class=\"flex\">\n                    <div class=\"flex-auto\">\n                        <a href=\"#/show/".concat(NEWSFEED[i].id, "\">\n                            ").concat(NEWSFEED[i].title, "\n                        </a>\n                    </div>\n                    <div class=\"text-center text-sm\">\n                        <div class=\"w-10 text-white bg-green-300 rounded-lg px-0 py-2\">\n                            (").concat(NEWSFEED[i].comments_count, ")\n                        </div>\n                    </div>\n                </div>\n                <div class=\"flex mt-3\">\n                    <div class=\"grid grid-cols-3 text-sm text-gray-500\">\n                        <div>\n                            <i class=\"fas fa-user mr-1\"></i>").concat(NEWSFEED[i].user, "\n                        </div>\n                        <div>\n                            <i class=\"fas fa-heart mr-1\"></i>").concat(NEWSFEED[i].points, "\n                        </div>    \n                        <div>\n                            <i class=\"fas fa-clock mr-1\"></i>").concat(NEWSFEED[i].time_ago, "\n                        </div>        \n                    </div>\n                </div>\n            </div>\n        "));
   }
   template = template.replace('{{__news_feed__}}', NEWSLIST.join(''));
   template = template.replace('{{__prev_page__}}', STORE.currentPage > 1 ? STORE.currentPage - 1 : 1);
@@ -149,7 +149,20 @@ function newsFeed() {
 function newsDetail() {
   var id = location.hash.substring(7); // 내가 쓰고 싶은 위치 값을 지정해주면 된다. 그 이후의 나머지 문자열들만 반환한다.
   var NEWSCONTENT = getData(CONTENT_URL.replace('@id', id));
-  CONTAINER.innerHTML = "\n        <h1>".concat(NEWSCONTENT.title, "</h1>\n\n        <div>\n            <a href=\"#/page/").concat(STORE.currentPage, "\">\uBAA9\uB85D\uC73C\uB85C</a>\n        </div>    \n    ");
+  var template = "\n        <div class=\"bg-gray-600 min-h-screen pb-8\">\n            <div class=\"bg-white text-xl\">\n                <div class=\"mx-auto px-4\">\n                    <div class=\"flex justify-between items-center py-6\">\n                        <div class=\"flex justify-start\">\n                            <h1 class=\"font-extrabold\">\n                                HACKER NEWS\n                            </h1>\n                        </div>\n                        <div class=\"items-center justify-end\">\n                            <a href=\"#/page/".concat(STORE.currentPage, "\" class=\"text-gray-500\">\n                                <i class=\"fa fa-times\"></i>\n                            </a>\n                        </div>\n                    </div>\n                </div>\n            </div>\n            <div class=\"h-full border rounded-xl bg-white m-6 p-4\">\n                <h2>\n                    ").concat(NEWSCONTENT.title, "\n                </h2>\n                <div class=\"text-gray-400 h-20\">\n                    ").concat(NEWSCONTENT.content, "\n                </div>\n                {{__comments__}}\n            </div>\n        </div>\n    ");
+  function makeComment(comments) {
+    var called = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+    var commentString = [];
+    for (var i = 0; i < comments.length; i++) {
+      commentString.push("\n                <div style=\"padding-left: ".concat(called * 40, "px\" class=\"mt-4\">\n                    <div class=\"text-gray-400\">\n                        <i class=\"fa fa-sort-up mr-2\"></i>\n                        <strong>").concat(comments[i].user, "</strong> ").concat(comments[i].time_ago, "\n                    </div>\n                    <p class=\"text-gray-700\">\n                        ").concat(comments[i].content, "\n                    </p>\n                </div>\n            "));
+      // 재귀 호출
+      if (comments[i].comments.length > 0) {
+        commentString.push(makeComment(comments[i].comments, called + 1));
+      }
+    }
+    return commentString.join('');
+  }
+  CONTAINER.innerHTML = template.replace('{{__comments__}}', makeComment(NEWSCONTENT.comments));
 }
 function router() {
   var routePath = location.hash;
